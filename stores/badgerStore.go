c9f923a57cb33bb2cbc8fb86b1d758dc4ecf5dae
Rob Taylor <roboncode@gmail.com>
2019-04-07 21:39:57 -0600

    partially working /links in badger

--- stores/badgerStore.go
+++ stores/badgerStore.go
@@ -14,8 +14,10 @@ const (
 )
 
 type BadgerStore struct {
-	db      *badger.DB
-	counter models.Counter
+	codeDB    *badger.DB
+	hashDB    *badger.DB
+	counterDB *badger.DB
+	counter   models.Counter
 }
 
 func NewBadgerStore() Store {
@@ -23,13 +25,16 @@ func NewBadgerStore() Store {
 	_ = viper.BindEnv(BadgerDir)
 
 	b := BadgerStore{}
-	b.db = b.connect()
+	b.codeDB = b.connect("code")
+	b.hashDB = b.connect("hash")
+	b.counterDB = b.connect("counter")
 	b.restoreCounter()
 	return &b
 }
 
-func (b *BadgerStore) connect() *badger.DB {
-	dir := viper.GetString(BadgerDir)
+func (b *BadgerStore) connect(database string) *badger.DB {
+	baseDir := viper.GetString(BadgerDir)
+	dir := baseDir + "/" + database
 	_ = os.MkdirAll(dir, os.ModePerm)
 
 	opts := badger.DefaultOptions
@@ -43,7 +48,7 @@ func (b *BadgerStore) connect() *badger.DB {
 }
 
 func (b *BadgerStore) restoreCounter() {
-	_ = b.db.View(func(txn *badger.Txn) error {
+	_ = b.counterDB.View(func(txn *badger.Txn) error {
 		var err error
 		var item *badger.Item
 		item, err = txn.Get([]byte("counter"))
@@ -65,7 +70,7 @@ func (b *BadgerStore) FindLink(hashedLongUrl string) *models.Link {
 	var link *models.Link
 	var err error
 
-	err = b.db.View(func(txn *badger.Txn) error {
+	err = b.codeDB.View(func(txn *badger.Txn) error {
 		var err error
 		var item *badger.Item
 		item, err = txn.Get([]byte(hashedLongUrl))
@@ -93,7 +98,7 @@ func (b *BadgerStore) FindLink(hashedLongUrl string) *models.Link {
 
 func (b *BadgerStore) IncCount() int {
 	b.counter.Value++
-	_ = b.db.Update(func(txn *badger.Txn) error {
+	_ = b.counterDB.Update(func(txn *badger.Txn) error {
 		return txn.Set([]byte("counter"), b.counter.EncodeCounter())
 	})
 	return b.counter.Value
@@ -114,12 +119,14 @@ func (b *BadgerStore) Create(code string, longUrl string) (*models.Link, error) 
 		Created: time.Now(),
 	}
 
-	err = b.db.Update(func(txn *badger.Txn) error {
-		encodedLink := newLink.EncodeLink()
-		err = txn.Set([]byte(code), encodedLink)
+	encodedLink := newLink.EncodeLink()
 
-		err = txn.Set([]byte(hashedLongUrl), encodedLink)
-		return err
+	err = b.codeDB.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(code), encodedLink)
+	})
+
+	err = b.hashDB.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(hashedLongUrl), encodedLink)
 	})
 
 	if err != nil {
@@ -134,7 +141,7 @@ func (b *BadgerStore) Read(code string) (*models.Link, error) {
 	var link *models.Link
 	var err error
 
-	err = b.db.View(func(txn *badger.Txn) error {
+	err = b.codeDB.View(func(txn *badger.Txn) error {
 		var err error
 		var item *badger.Item
 		item, err = txn.Get([]byte(code))
@@ -162,16 +169,53 @@ func (b *BadgerStore) Read(code string) (*models.Link, error) {
 }
 
 func (b *BadgerStore) List(limit int, skip int) []models.Link {
-	return make([]models.Link, 0)
+	var link *models.Link
+	links := make([]models.Link, 0)
+
+	err := b.codeDB.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = limit
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		//prefix := []byte("k1pGP")
+		prefix := []byte("")
+		//for it.Rewind(); it.Valid(); it.Next() {
+		//for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+		for it.Seek(prefix); it.Valid(); it.Next() {
+			item := it.Item()
+			k := item.Key()
+			v, _ := item.Value()
+
+			//fmt.Printf("key=%s, value=%s\n", k, v)
+
+			link, _ = models.DecodeLink(v)
+			link.ID = k
+			link.ShortUrl = helpers.GetShortUrl(link.Code)
+			links = append(links, *link)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return links
+	}
+
+	return links
 }
 
 func (b *BadgerStore) Delete(code string) int {
 	link, _ := b.Read(code)
 	var err error
 	if link != nil {
-		err = b.db.Update(func(txn *badger.Txn) error {
-			hashedLongUrl := helpers.MD5(link.LongUrl)
+		err = b.codeDB.Update(func(txn *badger.Txn) error {
 			err = txn.Delete([]byte(code))
+			return err
+		})
+		if err != nil {
+			return 0
+		}
+		err = b.hashDB.Update(func(txn *badger.Txn) error {
+			hashedLongUrl := helpers.MD5(link.LongUrl)
 			err = txn.Delete([]byte(hashedLongUrl))
 			return err
 		})
